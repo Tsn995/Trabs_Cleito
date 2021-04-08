b979357c70ce0a6ad4ebd42c4477f245de0cad92
Diogo Gil <60978019+Tsn995@users.noreply.github.com>
2021-04-08 20:02:58 -0300

    Add files via upload

---
+++ array_filter.js
@@ -0,0 +1,13 @@
+function filter(array) {
+    for (let i = 0; i < array.length; i++) {
+        document.write(i.valueOf > 52);
+    }
+}
+
+randArray = new array[10];
+
+randArray.forEach(function () {
+    element = Math.random() * 100;
+    randArray.push(element);
+    console.log(randArray[i]);
+});
